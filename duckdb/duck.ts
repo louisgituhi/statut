425a3394f66325356f16a2aa0d1b26138b1a7811
louis <louisgituhi@gmail.com>
2025-08-23 21:44:44 +0300

    duck stuff

--- duckdb/duck.ts
+++ duckdb/duck.ts
@@ -1,11 +1,5 @@
 import { DuckDBInstance } from "@duckdb/node-api";
 
-async function main() {
-	const instance = await DuckDBInstance.create('statut.db');
-	const connection = await instance.connect();
-
-	await connection.run("INSTALL postgres");
-	await connection.run("LOAD postgres");
-
-	const connectionString = process.env.DATABASE_URL as string;
+async function createSecretsTable() {
+	const instance = await DuckDBInstance.create();
 }
